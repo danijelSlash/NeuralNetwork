62286d80421d813f844ade792ed1e0d0def92870
Danijel <danijel.pavlek@fer.hr>
2016-07-19 09:50:19 +0200

    Starting commit

---
+++ src/AbstractNeuron.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Danijel on 7/19/2016.
+ */
+public abstract class AbstractNeuron {
+}
---
+++ src/OutputNeuron.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Danijel on 7/19/2016.
+ */
+public class OutputNeuron extends AbstractNeuron {
+
+}
